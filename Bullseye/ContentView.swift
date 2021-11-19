d1bc02ecb36d5a7d719535ddd1d3a711f27af7f6
Bea Totanes <36788217+beatricetotanes@users.noreply.github.com>
2021-11-19 21:39:59 +0800

    Updated

--- Bullseye/ContentView.swift
+++ Bullseye/ContentView.swift
@@ -7,15 +7,35 @@
 
 import SwiftUI
 
+// Main view of the app
 struct ContentView: View {
-    var body: some View {
-        Text("Hello, world!")
-            .padding()
+  var body: some View {
+    VStack {
+      Text("🎯🎯🎯\nPUT THE BULLSEYE AS CLOSE AS YOU CAN TO ")
+        .bold()
+        .kerning(2.0)
+        .multilineTextAlignment(.center)
+        .lineSpacing(4)
+        .font(.footnote)
+      
+      Text("89")
+      HStack {
+        Text("1")
+        Slider(value: .constant(50.0), in: 1.0...100.0)
+        Text("100")
+      }
+      Button(action: {}) {
+        Text("Hit Me")
+      }
     }
+  }
 }
 
 struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
+  static var previews: some View {
+    ContentView()
+    
+    ContentView()
+      .previewLayout(.fixed(width: 568, height: 320))
+  }
 }
