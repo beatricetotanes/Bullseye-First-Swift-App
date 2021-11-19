7e10c4fe7200703e3714e46c6678eca25b6e9835
Bea Totanes <36788217+beatricetotanes@users.noreply.github.com>
2021-11-19 21:37:35 +0800

    Initial Commit

---
+++ Bullseye/BullseyeApp.swift
@@ -0,0 +1,17 @@
+//
+//  BullseyeApp.swift
+//  Bullseye
+//
+//  Created by Beatrice Totanes on 11/19/21.
+//
+
+import SwiftUI
+
+@main
+struct BullseyeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
