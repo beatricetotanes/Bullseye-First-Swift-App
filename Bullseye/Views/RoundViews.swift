22efb27bbc6935c233f0e0f0f06260933a87ef44
Bea Totanes <36788217+beatricetotanes@users.noreply.github.com>
2021-12-22 22:21:38 +0800

    Put stroke and fill in icons

---
+++ Bullseye/Views/RoundViews.swift
@@ -0,0 +1,55 @@
+//
+//  RoundViews.swift
+//  Bullseye
+//
+//  Created by Beatrice Totanes on 12/20/21.
+//
+
+import SwiftUI
+
+struct RoundedImageViewStroked: View {
+  var systemName: String // name of the SF symbols we're trying to use
+  
+  var body: some View {
+    // symbol through image; systemName allows you to pass the name of a SFSymbol
+    Image(systemName: systemName)
+      .font(.title)
+      .foregroundColor(Color("TextColor"))
+      .frame(width: 56, height: 56)
+      .overlay(Circle()
+                .strokeBorder(Color("ButtonStrokeColor"), lineWidth: 2.0)
+      )
+  }
+}
+
+struct RoundedImageViewFilled: View {
+  var systemName: String // name of the SF symbols we're trying to use
+  
+  var body: some View {
+    // symbol through image; systemName allows you to pass the name of a SFSymbol
+    Image(systemName: systemName)
+      .font(.title)
+      .foregroundColor(Color("ButtonFilledTextColor"))
+      .frame(width: 56, height: 56)
+      .background(Circle()
+                    .fill(Color("ButtonFilledBackgroundColor")))
+  }
+}
+
+struct PreviewView: View {
+  var body: some View{
+    // you can put spacing (optional) instead of having to put padding one by one
+    VStack (spacing: 10){
+      RoundedImageViewStroked(systemName: "arrow.counterclockwise")
+      RoundedImageViewFilled(systemName: "list.dash")
+    }
+  }
+}
+
+struct RoundViews_Previews: PreviewProvider {
+  static var previews: some View {
+    PreviewView()
+    PreviewView()
+      .preferredColorScheme(.dark)
+  }
+}
